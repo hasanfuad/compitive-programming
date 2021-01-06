efb21bc95a14df382022861d343a3f18137a8ca5
hasanfuad <43511316+hasanfuad@users.noreply.github.com>
2021-01-06 21:14:33 +0600

    area of a range

---
+++ rangeOfaCircle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#define pi 3.1416
+using namespace std;
+
+int main(){
+	int radius;
+	cin >> radius;
+
+	float areaOfCircle = pi* pow(radius,2);
+
+	cout<< areaOfCircle;
+
+}
